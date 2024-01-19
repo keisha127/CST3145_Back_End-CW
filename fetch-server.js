b65603bc5d97d7e7351d940d56538e668035477f
keisha127 <keishaowino712@gmail.com>
2024-01-19 13:07:30 +0000

    main server js file

---
+++ fetch-server.js
@@ -0,0 +1,51 @@
+
+const express = require("express");
+const cors = require("cors");
+const morgan = require("morgan");
+const path = require("path");
+
+
+const app = express();
+
+let propertiesReader = require("properties-reader");
+let propertiesPath = path.resolve(__dirname, "conf/db.properties");
+let properties = propertiesReader(propertiesPath);
+
+let dbPrefix = properties.get("db.prefix");
+
+let dbUsername = encodeURIComponent(properties.get("db.user"));
+let dbPwd = encodeURIComponent(properties.get("db.pwd"));
+let dbName = properties.get("db.dbName");
+let dbUrl = properties.get("db.dbUrl");
+let dbParams = properties.get("db.params");
+
+const uri = dbPrefix + dbUsername + ":" + dbPwd + dbUrl + dbParams;
+
+
+// const uri = "mongodb+srv://WebstoreUser:<cst3145>@webstorecluster.gwgxspe.mongodb.net/?retryWrites=true&w=majority";
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
+let db = client.db(dbName);
+
+
+app.use(express.json());
+
+app.param('collectionName', function(req, res, next, collectionName) {
+    req.collection = db.collection(collectionName);
+    return next();
+});
+
+app.get('/collections/:collectionName', function(req, res, next) {
+    req.collection.find({}).toArray(function(err, results) {
+        if (err) {
+            return next(err);
+        }
+        res.send(results);
+    });
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, function() {
+    console.log("App on " + port );
+});
