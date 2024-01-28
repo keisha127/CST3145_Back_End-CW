e1cfc960a6467cb5c7e15f9bbb034c868b891840
keisha127 <keishaowino712@gmail.com>
2024-01-28 20:04:02 +0000

    update

--- fetch-server.js
+++ fetch-server.js
@@ -1,87 +1,75 @@
-
-const express = require("express");
-const cors = require("cors");
-const morgan = require("morgan");
-const path = require("path");
+var express = require("express");
+var Mongoclient=require("mongodb").MongoClient;
+var cors = require("cors");
 
 
-const app = express();
-
-let propertiesReader = require("properties-reader");
-let propertiesPath = path.resolve(__dirname, "conf/db.properties");
-let properties = propertiesReader(propertiesPath);
-
-let dbPrefix = properties.get("db.prefix");
-
-let dbUsername = encodeURIComponent(properties.get("db.user"));
-let dbPwd = encodeURIComponent(properties.get("db.pwd"));
-let dbName = properties.get("db.dbName");
-let dbUrl = properties.get("db.dbUrl");
-let dbParams = properties.get("db.params");
-
-const uri = dbPrefix + dbUsername + ":" + dbPwd + dbUrl + dbParams;
-
-
-// const uri = "mongodb+srv://WebstoreUser:<cst3145>@webstorecluster.gwgxspe.mongodb.net/?retryWrites=true&w=majority";
-
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
-let db = client.db(dbName);
-
-
+var app = express();
+app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.param('collectionName', function(req, res, next, collectionName) {
-    req.collection = db.collection(collectionName);
-    return next();
-});
+var CONNECTION_STRING = "mongodb+srv://WebstoreUser:cst3145@webstorecluster.gwgxspe.mongodb.net/"
 
-app.get('/collections/:collectionName', function(req, res, next) {
-    req.collection.find({}).toArray(function(err, results) {
-        if (err) {
-            return next(err);
-        }
-        res.send(results);
+var databaseName = "Webstore";
+var database;
+
+app.listen(3000, () => {
+    Mongoclient.connect(CONNECTION_STRING, (error, client) => {
+        database = client.db(databaseName);
+        console.log("connection to database was successful");
     });
-});
+})
 
-app.post('/collections/:collectionName', function(req, res, next) {
-    // validate req.body
-    req.collection.insertOne(req.body, function(err, results) {
-        if (err) {
-            return next(err);
-        }
-        res.send(results);
+app.get('/api/storeapp/getLessons', (req, res) => {
+    database.collection("lessons").find({}).toArray((error, result) => {
+        res.send(result);
     });
-});
+})
 
-app.put('/collections/:collectionName/:id', function(req, res, next) {
-    res.collection.updateOne({_id: new ObjectId(req.params.id)},
-    {$set: req.body},
-    {safe: true, multi: false}, function(err, result) {
-        if (err) {
-            return next(err);
+app.get('/api/storeapp/getOrders', (req, res) => {
+    database.collection("orders").find({}).toArray((error, result) => {
+        res.send(result);
+    });
+})
+
+
+app.post('/api/storeapp/addOrder', (req, res) => {
+    const order = req.body;
+
+    // save to 'orders' collection
+    database.collection("orders").insertOne({
+        firstName: order.firstName,
+        lastName: order.lastName,
+        phoneNumber: order.phoneNumber,
+        lessons: order.cart,
+        orderDate: new Date(),
+    }, (error, result) => {
+        if (error) {
+            console.error('Error saving order:', error);
+            res.status(500).send('Internal Server Error');
         } else {
-            res.send((result.matchedCount === 1) ? {msg: "success"} : {msg: "error"});
+            // if order successfully saved, update lessons
+            const cartLessons = order.cart;
+
+            if (cartLessons && cartLessons.length > 0) {
+                const lessonIds = cartLessons.map(lesson => lesson._id);
+
+                // Update 'lessons' collection
+                database.collection("lessons").updateMany(
+                    { _id: { $in: lessonIds } },
+                    { $inc: { spaces: -1 } }, // Decrease spaces for each lesson 
+                    (updateError, updateResult) => {
+                        if (updateError) {
+                            console.error('Error updating lessons in the cart:', updateError);
+                            res.status(500).send('Internal Server Error');
+                        } else {
+                            res.status(201).send('Order submitted successfully');
+                        }
+                    }
+                );
+            } else {
+                res.status(201).send('Order submitted successfully');
+            }
         }
     });
-});
-
-app.delete('/collections/:collectionName/:id', function(req, res, next) {
-    // validate req.body
-    req.collection.deleteOne(
-        {_id: new ObjectId(req.params.id)}, function(err, results) {
-        if (err) {
-            return next(err);
-        } else {
-        res.send((results.deleteCount === 1) ? {msg: "success"} : {msg: "error"});
-        }
-    });
-});
-
-
-
-const port = process.env.PORT || 3000;
-app.listen(port, function() {
-    console.log("App on " + port );
 });
